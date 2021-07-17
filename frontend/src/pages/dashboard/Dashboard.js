3a8fbaaddcea4adc5bdb3885a7096025eb16ddf2
THeMuCiT127 <57565068+THeMuCiT127@users.noreply.github.com>
2021-07-17 16:13:41 +0300

    update5

--- frontend/src/pages/dashboard/Dashboard.js
+++ frontend/src/pages/dashboard/Dashboard.js
@@ -69,7 +69,7 @@ const Dashboard = () => {
                          </div>
                     </div>
                     <div className='dashboardRight'>
-                         <div className='dashboardSmallTitle'>Calendar</div>
+                         <div className='dashboardSmallTitle'>Calendars</div>
                          <div className='dashboardCalendarWrapper'>
                               <div className='dashboardCalendar'>
                                    <Calendar />
